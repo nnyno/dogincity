9f1d1d2811577a11f569fb5e567372bf33d2902a
unanbb <86473590+unanbb@users.noreply.github.com>
2022-05-27 12:03:13 +0900

    AI
    AI 사람

--- Assets/1.Scripts/Item.cs
+++ Assets/1.Scripts/Item.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public enum Type {apple, banana, obj};
+    public enum Type {apple, banana, obj,waypoint};
     public Type type;
     public int value;
 }
---
+++ Assets/characters_6_09/colli.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colli : MonoBehaviour
+{
+    public bool waypoints = true;
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("waypoint"))
+        {
+            waypoints = true;
+        }
+    }
+}
