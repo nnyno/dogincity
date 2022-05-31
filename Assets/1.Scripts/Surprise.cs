43a3cb9aab1fc699f76015b2259cc3682d5bb3bd
nnyno <alsgh0891@gmail.com>
2022-06-01 02:16:10 +0900

    111

---
+++ Assets/1.Scripts/Surprise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surprise : MonoBehaviour
+{
+    public difficulty difficulty = null;
+    Animator _animator;
+
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(difficulty.surprise == true)
+        {
+            _animator.SetTrigger("surprised");
+            difficulty.surprise = false;
+        }
+    }
+}
