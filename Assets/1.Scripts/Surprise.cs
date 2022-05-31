801b27eed3783866fef673888b6b4700512c492c
nnyno <alsgh0891@gmail.com>
2022-06-01 02:34:21 +0900

    사람 놀라는 애니메이션

--- Assets/1.Scripts/Surprise.cs
+++ Assets/1.Scripts/Surprise.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Surprise : MonoBehaviour
 {
     public difficulty difficulty = null;
+    public NavMeshAgent agent;
     Animator _animator;
 
     void Start()
     {
         _animator = this.GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
@@ -17,7 +20,13 @@ public class Surprise : MonoBehaviour
         if(difficulty.surprise == true)
         {
             _animator.SetTrigger("surprised");
+            agent.speed = 0f;
             difficulty.surprise = false;
         }
+    }
+
+    public void humango()
+    {
+        agent.speed = 3.5f;
     }
 }
