6f56234d926b5960b8bd5a65752a17243fb65f9c
nnyno <alsgh0891@gmail.com>
2022-06-01 15:19:13 +0900

    home에러 제거 사람 걷는 애니메이션 수정
    male1만 수정

---
+++ Assets/1.Scripts/AiAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiAnim : MonoBehaviour
+{
+    public difficulty difficulty = null;
+    public NavMeshAgent agent;
+    Animator _animator;
+
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(difficulty.surprise == true)
+        {
+            _animator.SetTrigger("surprised");
+            agent.speed = 0f;
+            difficulty.surprise = false;
+        }
+        _animator.SetFloat("Blend", agent.speed + 0.2f * 0.1f, 0.1f, Time.deltaTime);
+    }
+
+    public void humango()
+    {
+        agent.speed = 3.5f;
+    }
+}
