90c8f027dcc33918ff2c91cb6bcc1491d672705c
unanbb <86473590+unanbb@users.noreply.github.com>
2022-09-28 14:15:15 +0900

    마켓, 시장 sliced map 랜덤배치
    쓰레기 mass:1 drag:3 angular drag:0.5 scale 조정

---
+++ Assets/1.Scripts/Random_market.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_market : MonoBehaviour
+{
+    public Transform[] spawnPosArray;
+    public GameObject slicedMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(slicedMap, spawnPosArray[Random.Range(0, 10)]);
+    }
+}
