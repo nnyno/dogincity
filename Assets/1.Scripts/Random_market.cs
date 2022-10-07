2693be0e688ff5f8bef6aba71bdc45e5b8d690ee
unanbb <86473590+unanbb@users.noreply.github.com>
2022-10-08 01:48:03 +0900

    market, park에 지도조각 2개드랍
    야매로 짬ㅋ

--- Assets/1.Scripts/Random_market.cs
+++ Assets/1.Scripts/Random_market.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Random_market : MonoBehaviour
 {
+    private int count;//
+    private int count2;// 
     public Transform[] spawnPosArray;
     public GameObject slicedMap;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(slicedMap, spawnPosArray[Random.Range(0, 10)]);
+        count = Random.Range(0, 10);//
+        Instantiate(slicedMap, spawnPosArray[count]);//Instantiate(slicedMap, spawnPosArray[Random.Range(0,10));
+        count2 = Random.Range(0, 10);//
+        for (int i = 0; i < 100; i++)//
+        {
+            if (count != count2)
+            {
+                Instantiate(slicedMap, spawnPosArray[count2]);
+                break;
+            }
+            else
+            {
+                count2 = Random.Range(0, 10);
+            }
+        }
+
     }
 }
